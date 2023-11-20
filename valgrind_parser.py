22a865fd6b1b8ff9c28d835c01deffbba7a45db2
Braden Helmer <bradenhelmeraus@gmail.com>
2023-11-20 12:52:34 -0500

    Valgrind complete

--- valgrind_parser.py
+++ valgrind_parser.py
@@ -3,15 +3,25 @@
 import sys
 
 if __name__ == "__main__":
-
-    # Get file name 
+    # Get args
     args = sys.argv
 
     if len(args) < 2:
         raise ValueError("No file name to parse!")
 
+    # Get filename
     filename = args[1]
 
-    print(filename)
-
-    
+    # Open file for parsing
+    with open(filename, "r") as valgrind_file:
+        if valgrind_file.closed:
+            raise FileNotFoundError("Error opening Valgrind file for parsing!")
+        lines = valgrind_file.readlines()
+        for line in lines:
+            words = line.split()
+            if "Collected" in words:
+                instruction_count = words[-1]
+                print(
+                    f"The total number of executed instructions for the program was: {instruction_count}"
+                )
+        valgrind_file.close()
