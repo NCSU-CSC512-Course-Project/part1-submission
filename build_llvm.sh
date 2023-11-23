a960ae9db0bea28698665fb17cec22b79836b344
Braden Helmer <bradenhelmeraus@gmail.com>
2023-11-22 22:03:54 -0500

    LLVM Script test

--- build_llvm.sh
+++ build_llvm.sh
@@ -1,1 +1,13 @@
 # Script for building LLVM with LibClang (Clang tools Extra).
+
+# Ensure CMake and Ninja are Installed
+sudo apt update
+sudo apt -y install cmake ninja-build
+
+# Install LLVM
+git clone https://github.com/llvm/llvm-project.git
+cd llvm-project
+mkdir build
+cmake -B build -S llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang"
+sudo ninja -C build install -j$(nproc)
+
