b0a9df3032b3a159d23b5310adec16121d964de5
Braden Helmer <bradenhelmeraus@gmail.com>
2023-11-27 12:40:40 -0500

    Update TF_3_SPEC.c

--- TF_3_SPEC.c
+++ TF_3_SPEC.c
@@ -66,7 +66,6 @@ static int mti = N + 1;     /* mti==N+1 means mt[N] is not initialized */
 /* initializes mt[N] with a seed */
 CLINK void spec_init_genrand(unsigned long s) {
   mt[0] = s & 0xffffffffUL;
-#include <stdio.h>
   for (mti = 1; mti < N; mti++) {
     mt[mti] = (1812433253UL * (mt[mti - 1] ^ (mt[mti - 1] >> 30)) + mti);
     /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
